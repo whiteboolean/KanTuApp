ffd235822a05e4866dd5fa1e74175e95ab73e57f
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-09-23 22:25:56 +0800

    add

---
+++ KanTu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KanTu
+//
+//  Created by D G on 2022/9/23.
+//
+
+import UIKit
+
+/**
+ *  看图项目
+ */
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
---
+++ KanTuUITests/KanTuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KanTuUITestsLaunchTests.swift
+//  KanTuUITests
+//
+//  Created by D G on 2022/9/23.
+//
+
+import XCTest
+
+final class KanTuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
